a6d312c77dd7d21660a789596562761e0697261b
SkoSC <scorpius98.alex@gmail.com>
2020-02-29 08:13:10 -0800

    UPDATE 4

--- Test/TextSplitterTest.cs
+++ Test/TextSplitterTest.cs
@@ -12,9 +12,27 @@ namespace Test
 
         [TestCase("hello world", new String[] {"hello", "world"})]
         [TestCase("hello        world", new String[] { "hello", "world" })]
-        [TestCase("hello world,!", new String[] { "hello", "world", "!" })]
         [TestCase("", new String[] { })]
+        public void DefaultImplementationOnlyWhitespace(String input, String[] output)
+        {
+            DefaultImplementation(input, output);
+        }
+
+        [TestCase("helloworld,!", new String[] { "helloworld", "!" })]
+        [TestCase("hello,world,!", new String[] { "hello", "world", "!" })]
+        [TestCase(",", new String[] {  })]
+        public void DefaultImplementationOnlyComma(String input, String[] output)
+        {
+            DefaultImplementation(input, output);
+        }
+
+        [TestCase("hello world,!", new String[] { "hello", "world", "!" })]
         [TestCase("    ,,   ", new String[] { })]
+        public void DefaultImplementationMixed(String input, String[] output)
+        {
+            DefaultImplementation(input, output);
+        }
+
         public void DefaultImplementation(String input, String[] output)
         {
             Assert.AreEqual(output, _splitter.Split(input));
